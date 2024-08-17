1162ba44e35e5314611720fb258ba59124bad112
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-17 17:56:54 -0500

    Update Ejercicio1.js

--- Ejercicio1/Ejercicio1.js
+++ Ejercicio1/Ejercicio1.js
@@ -16,4 +16,17 @@ else{
     alert("Señor o señora "+ PriV + " El numero de horas es " + SegV + " Y su salario equivale a " + X)
 }
 
+// Funcion
+
+function calcularSalario(empleado, horas, salario){
+     empleado = prompt("Ingrese su nombre")
+     horas = Number(prompt("Ingresa el numero de horas que has trabajado"))
+     salario = 0
+if(horas < 10){
+    salario=(horas * 30000)
+}else salario=(horas * 33000)
+
+alert(`Señor ${empleado} el numero de horas es ${horas} y Su salario equivale a: ${salario}`)
+}
+calcularSalario()
 
