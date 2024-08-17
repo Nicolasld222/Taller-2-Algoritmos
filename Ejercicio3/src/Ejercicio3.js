265e5dc4cb26fda3a1b21636b0963f07161ba469
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-17 18:03:07 -0500

    Update Ejercicio3.js

--- Ejercicio3/src/Ejercicio3.js
+++ Ejercicio3/src/Ejercicio3.js
@@ -32,3 +32,72 @@ else if (one === 2) {
     alert("Opción inválida. Por favor, elige 1 para nevera grande o 2 para nevera pequeña.");
 }
 
+
+
+
+
+//Funcion
+
+
+
+
+alert("Hola, bienvenido a nuestra página para alquilar lavadoras. El precio por hora será Grande: 4000, Pequeña: 3000");
+
+//Lavadora escogida
+function TipoDeLavadora(){
+    let tipoLava = Number(prompt("¿Qué tipo de lavadoras deseas alquilar? Ingrese 1) Grandes 2) Pequeñas"));
+    
+    if(tipoLava == 1){
+        return 'Grande';
+    } else if(tipoLava == 2){
+        return 'Pequeña';
+    } else {
+        return null;
+    }
+}
+
+//Precio de la lavadora
+function ValorLavadora(tipoDeLavadora){
+    if(tipoDeLavadora === 'Grande'){
+        return 4000;
+    } else if(tipoDeLavadora === 'Pequeña'){
+        return 3000;
+    } else {
+        return 0;
+    }
+}
+
+//Cantidad de lavadoras
+function CantidadLav(){
+    return Number(prompt("¿Cuántas lavadoras deseas alquilar?"));
+}
+
+//Cuántas Horas
+function HorasAlquiladas(){
+    return Number(prompt("¿Durante cuántas horas las deseas alquilar?"));
+}
+
+//Descuento
+function Descuento(valorLavadora, cantidadLav, horasAlquiladas){
+    let costoTotal = valorLavadora * cantidadLav * horasAlquiladas;
+    
+    if(cantidadLav > 3){
+        let descuento = costoTotal * 0.03;
+        costoTotal -= descuento;
+    }
+    
+    return costoTotal;
+}
+
+function Principal(){
+    let tipo = TipoDeLavadora();
+    let valor = ValorLavadora(tipo);
+    let cantidad = CantidadLav();
+    let horas = HorasAlquiladas();
+    let costoTotal = Descuento(valor, cantidad, horas);
+    
+    alert(`Su tipo de lavadora es ${tipo}. La cantidad que vas a alquilar es ${cantidad}. El costo total va a ser de ${costoTotal}.`);
+}
+
+Principal();
+
