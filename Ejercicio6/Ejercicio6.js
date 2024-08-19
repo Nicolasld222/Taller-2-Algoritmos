a5a880916e18b467af13190633476014b2b9df3e
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-19 12:06:07 -0500

    Update Ejercicio6.js

--- Ejercicio6/Ejercicio6.js
+++ Ejercicio6/Ejercicio6.js
@@ -19,3 +19,28 @@ if (x > numero3) {
     
 }
 alert ("tu numero mayor es "+ mayor)
+
+
+//Función para obtener los números
+function NumerosEscogidos() {
+    let numero1 = Number(prompt("Elige tu primer número"));
+    let numero2 = Number(prompt("Elige tu segundo número"));
+    let numero3 = Number(prompt("Elige tu tercer número"));
+
+    return [numero1, numero2, numero3]; 
+}
+
+// Función para identificar cuál es el número mayor
+function Numeromayor(numero1, numero2, numero3) {
+    return Math.max(numero1, numero2, numero3); // Usar Math.max para obtener el mayor y Math.min para el menor
+}
+
+// Función Principal
+function Principal() {
+    let numeros = NumerosEscogidos(); 
+    let mayor = Numeromayor(numeros[0], numeros[1], numeros[2]); 
+
+    alert(`Tu número mayor es ${mayor}`);
+}
+
+Principal();
