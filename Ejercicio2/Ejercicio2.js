a085b46cd725d7b9c34f964d000b338f0c2329a4
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-18 20:00:33 -0500

    Update Ejercicio2.js

--- Ejercicio2/Ejercicio2.js
+++ Ejercicio2/Ejercicio2.js
@@ -1,5 +1,5 @@
-export let X = window.prompt("Escribe un numero")
-x = 7
+let X = Number(prompt("Escribe un numero"));
+
 
 if(X >= 0) {
     alert("Su numero es positivo")
@@ -7,5 +7,35 @@ if(X >= 0) {
 else { 
     alert("Su numero es negativo")
 }
+
+//Funcion 
+
+// Función para obtener el número del usuario
+function NumeroElegido(){
+    let Num = Number(prompt("Escribe un número"));
+    return Num;
+}
+
+// Determinar si el número es positivo, negativo o neutro
+function NegPos(Num){
+    if(Num > 0) {
+        return "Positivo";
+    } else if(Num === 0){ 
+        return "Neutro";
+    } else {
+        return "Negativo";
+    }
+}
+
+// Función principal
+function Principal(){
+    let NumeroEscogido = NumeroElegido();
+    let NumeroPosNeg = NegPos(NumeroEscogido);
+
+    alert(`Su número es ${NumeroEscogido} y es ${NumeroPosNeg}`);
+}
+
+Principal();
+
 
 
