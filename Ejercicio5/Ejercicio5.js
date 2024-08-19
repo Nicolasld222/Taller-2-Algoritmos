6943992af0f7ed3e9564fcde0eabbafe8df4f96b
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-18 19:40:14 -0500

    Update Ejercicio5.js

--- Ejercicio5/Ejercicio5.js
+++ Ejercicio5/Ejercicio5.js
@@ -7,3 +7,68 @@ var quimica = window.prompt("Escribe tu nota de Quimica")
 const number = Promedio = (matematicas + Fisica + informatica + biologia + quimica) / 5
 
 alert(Promedio)
+
+// Funcion 
+
+//Matematicas
+function matematicas(){
+    let Notam = Number(prompt("Ingresar su nota en la materia de matematicas"));
+    return Notam;
+}
+
+//Fisica
+function fisica(){
+    let NotaF = Number(prompt("Ingresar su nota en la materia de fisica"));
+    return NotaF;
+}
+
+//Quimica
+function quimica(){
+    let Notaq = Number(prompt("Ingresar su nota en la materia de quimica"));
+    return Notaq;
+}
+
+//Biologia
+function biologia(){
+    let NotaB = Number(prompt("Ingresar su nota en la materia de biologia"));
+    return NotaB;
+}
+
+//Informatica
+function informatica(){
+    let Notai = Number(prompt("Ingresar su nota en la materia de informatica"));
+    return Notai;
+}
+
+// Funcion promedio
+function promedio(matematicas, fisica, quimica, biologia, informatica){
+    let promediototal = (matematicas + fisica + quimica + biologia + informatica) / 5;
+    return promediototal;
+}
+
+// Funcion calificación
+function calificacion(promedio){
+    if(promedio > 8){
+        return "Excelente";
+    } else if(promedio >= 6){
+        return "Bueno";
+    } else {
+        return "Malo";
+    }
+}
+
+// Funcion principal
+function principal(){
+    let notaMatematicas = matematicas();
+    let notaFisica = fisica();
+    let notaQuimica = quimica();
+    let notaBiologia = biologia();
+    let notaInformatica = informatica();
+
+    let promedioTotal = promedio(notaMatematicas, notaFisica, notaQuimica, notaBiologia, notaInformatica);
+    let calificacionFinal = calificacion(promedioTotal);
+
+    alert(`Su promedio de notas es ${promedioTotal} y su calificación es ${calificacionFinal}`);
+}
+
+principal();
