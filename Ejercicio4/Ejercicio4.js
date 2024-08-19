2481c758421e25c38d69a085cf456a1ce349dc1b
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-18 20:13:26 -0500

    Update Ejercicio4.js

--- Ejercicio4/Ejercicio4.js
+++ Ejercicio4/Ejercicio4.js
@@ -7,5 +7,30 @@ else{
     alert("Su numero es impar")
 }
 
-//Funcion
+// Funcion
+
+// Función para obtener el número del usuario
+function NumeroEscogido() {
+    return Number(prompt("Escoge un número"));
+}
+
+// Determinar si el número es par o impar
+function PosNeg(numero) {
+    if (numero % 2 === 0) {
+        return "Par";
+    } else {
+        return "Impar";
+    }
+}
+
+// Función principal
+function Principal() {
+    let numero = NumeroEscogido(); 
+    let numeroPosNeg = PosNeg(numero); 
+
+    alert(`Su número es ${numero} y es ${numeroPosNeg}`);
+}
+
+Principal(); 
+
 
