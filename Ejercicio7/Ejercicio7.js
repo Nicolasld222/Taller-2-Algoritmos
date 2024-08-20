eb7a0b5c8ff80dff894cec8cabc6c9e30e44cf5a
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-20 14:43:20 -0500

    Update Ejercicio7.js

--- Ejercicio7/Ejercicio7.js
+++ Ejercicio7/Ejercicio7.js
@@ -1,2 +1,12 @@
-let one = window.prompt("¿Que genero eres?")
+let Genero = Number(prompt("Escribe tu género: 1) Mujer 2) Hombre"));
+let Edad = Number(prompt("Escribe tu edad"));
 
+if (Edad >= 50 && Genero === 1) {
+    alert("Su mensualidad es de $120000");
+} else if (Edad >= 30 && Edad < 50 && Genero === 1) {
+    alert("Su mensualidad es de $100000");
+} else if (Edad >= 30 && Edad < 50 && Genero === 2) {
+    alert("Su mensualidad es de $40000");
+} else {
+    alert("No tienes ayuda de la alcaldía");
+}
