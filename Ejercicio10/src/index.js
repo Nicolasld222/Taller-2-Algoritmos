c4bcdbc2e2c430edf5396632e6df49825cdd4b88
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-21 18:13:24 -0500

    Update index.js

--- Ejercicio10/src/index.js
+++ Ejercicio10/src/index.js
@@ -15,3 +15,31 @@ if(impresion > 0 && impresion <= 499){
     alert("el valor de tu impresion es " + impresion * 50)
 }
 }
+
+// Función para obtener la cantidad de impresiones
+function Impresiones() {
+    return Number(prompt("Ingrese la cantidad de hojas que desea imprimir"));
+}
+
+// Función para calcular el precio de las impresiones
+function precio(impresiones) {
+    if (impresiones > 0 && impresiones <= 499) {
+        return impresiones * 120;
+    } else if (impresiones > 499 && impresiones <= 749) {
+        return impresiones * 100;
+    } else if (impresiones > 749 && impresiones <= 999) {
+        return impresiones * 80;
+    } else if (impresiones > 999) {
+        return impresiones * 50;
+    }
+}
+
+// Función principal para coordinar el flujo
+function Principal() {
+    let totalImpresion = Impresiones(); // Obtiene la cantidad de impresiones
+    let preciototal = precio(totalImpresion); // Calcula el precio basado en la cantidad de impresiones
+
+    alert(`La cantidad de impresiones es ${totalImpresion} y el total sería $${preciototal}`);
+}
+
+Principal(); // Llama a la función principal
